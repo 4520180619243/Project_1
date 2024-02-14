7027a4e0e5c7e5ca4918f3ee09906cee6f715d37
4520180619243 <156504943+4520180619243@users.noreply.github.com>
2024-02-14 12:11:57 +0500

    Add files via upload

---
+++ Famous_Quote.js
@@ -0,0 +1,5 @@
+"use strict";
+let famous_person = "Albert Einstein Once Said,";
+let famous_quote = famous_person + '\t"A         person who          never made a mistake never tried\n anything new"';
+console.log(famous_quote);
+console.log(5 + 3);
---
+++ Famous_Quote.ts
@@ -0,0 +1,7 @@
+let famous_person:string="Albert Einstein Once Said,";
+
+let famous_quote:string= famous_person+'\t"A         person who          never made a mistake never tried\n anything new"';
+
+console.log(famous_quote);
+console.log(5+3);
+
